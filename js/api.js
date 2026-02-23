e6e869d4ce8adc321a1dd3580f775efafb24f321
Elydeivison Cesar <163563034+DevElyShow@users.noreply.github.com>
2026-02-23 15:43:49 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 async function searchMovies(movieName) {
   try {
-    const response = await fetch(`${BASE_URL}&s=${movieName}`);
+    const response = await fetch(`${BASE_URL}&s=${encodeURIComponent(movieName)}`);
     const data = await response.json();
 
     if (data.Response === "False") {
